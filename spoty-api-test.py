dd4f7a463241fe505959c7bacf9c50e403187d6d
Stéfano Troncaro <stefanotroncaro@gmail.com>
2024-09-15 02:55:28 -0300

    Refactor to separate functions for legibility

--- spoty-api-test.py
+++ spoty-api-test.py
@@ -3,48 +3,67 @@ import json
 import requests
 
 SPOTIFY_TOKEN_ENDPOINT = "https://accounts.spotify.com/api/token"
+SPOTIFY_CLIENT_DATA_FIELDS = ["client_id", "client_secret"]
 
-# Get directories
-script_directory = Path(__file__).parent
-keys_directory = script_directory / "keys"
 
-# Check keys directory exists
-if not (keys_directory.exists() and keys_directory.is_dir()):
-    raise FileNotFoundError
+def GetKeysDirectory():
+    script_directory = Path(__file__).parent
+    keys_directory = script_directory / "keys"
+    if not (keys_directory.exists() and keys_directory.is_dir()):
+        raise FileNotFoundError()
+    return keys_directory
 
-# Get json objects in keys directory
-key_json_files = keys_directory.glob("*.json")
-if not key_json_files:
-    raise FileNotFoundError
 
-# Try each one for valid keys
-access_token_json = {}
-for json_file in key_json_files:
-    with json_file.open() as file:
-        keys: dict = json.load(file)
-        if not all(
-            required_field in keys for required_field in ["client_id", "client_secret"]
-        ):
-            print(f"{json_file.name} doesn't have required fields")
-            continue
+def GetKeysJsonFilePaths():
+    key_json_files = GetKeysDirectory().glob("*.json")
+    if not key_json_files:
+        raise FileNotFoundError()
+    return key_json_files
 
-        print(f"{json_file.name} has required fields")
 
-        # Request access token
-        keys["grant_type"] = "client_credentials"
-        response = requests.post(
-            SPOTIFY_TOKEN_ENDPOINT,
-            headers={"Content-Type": "application/x-www-form-urlencoded"},
-            data=keys,
-            timeout=10,
-        )
+def GetClientDataFromJson(path: Path):
+    with path.open() as file:
+        data: dict = json.load(file)
+    try:
+        values = (data[field] for field in SPOTIFY_CLIENT_DATA_FIELDS)
+    except KeyError:
+        print(f"{path.name} is invalid")
+        return None
 
-        # Handle failure
-        if response.status_code != 200:
-            print("Could not get access token. Got:")
-            print(f"  STATUS {response.status_code}\n  {response.text}")
-            continue
+    return {key: value for key, value in zip(SPOTIFY_CLIENT_DATA_FIELDS, values)}
 
-        # Success
-        print("Got access token!")
-        access_token_json = response.json()
+
+def RequestAccessToken(client_data: dict | None):
+    if not client_data:
+        return None
+
+    client_data["grant_type"] = "client_credentials"
+    response = requests.post(
+        SPOTIFY_TOKEN_ENDPOINT,
+        headers={"Content-Type": "application/x-www-form-urlencoded"},
+        data=client_data,
+        timeout=10,
+    )
+
+    if response.status_code != 200:
+        print("Could not get access token. Got:")
+        print(f"  STATUS {response.status_code}\n  {response.text}")
+        return None
+
+    access_token: str = response.json()["access_token"]
+    return access_token
+
+
+def GetAccessToken():
+    for key_json in GetKeysJsonFilePaths():
+        client_data = GetClientDataFromJson(key_json)
+        access_token = RequestAccessToken(client_data)
+        if access_token:
+            return access_token
+
+    raise RuntimeError("Couldn't get an access token")
+
+
+if __name__ == "__main__":
+    access_token = GetAccessToken()
+    print(f"Got access token: {access_token}")
