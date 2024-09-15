d55046e290f4666767f525a86da788d0b694f452
Stéfano Troncaro <stefanotroncaro@gmail.com>
2024-09-15 01:10:29 -0300

    Refactor reducing indentation for legibility

--- spoty-api-test.py
+++ spoty-api-test.py
@@ -22,29 +22,29 @@ access_token_json = {}
 for json_file in key_json_files:
     with json_file.open() as file:
         keys: dict = json.load(file)
-        if all(
+        if not all(
             required_field in keys for required_field in ["client_id", "client_secret"]
         ):
-            print(f"{json_file.name} has required fields")
-
-            # Request access token
-            keys["grant_type"] = "client_credentials"
-            response = requests.post(
-                SPOTIFY_TOKEN_ENDPOINT,
-                headers={"Content-Type": "application/x-www-form-urlencoded"},
-                data=keys,
-                timeout=10,
-            )
-
-            # Handle failure
-            if response.status_code != 200:
-                print("Could not get access token. Got:")
-                print(f"  STATUS {response.status_code}\n  {response.text}")
-                continue
-
-            # Success
-            print("Got access token!")
-            access_token_json = response.json()
-
-        else:
             print(f"{json_file.name} doesn't have required fields")
+            continue
+
+        print(f"{json_file.name} has required fields")
+
+        # Request access token
+        keys["grant_type"] = "client_credentials"
+        response = requests.post(
+            SPOTIFY_TOKEN_ENDPOINT,
+            headers={"Content-Type": "application/x-www-form-urlencoded"},
+            data=keys,
+            timeout=10,
+        )
+
+        # Handle failure
+        if response.status_code != 200:
+            print("Could not get access token. Got:")
+            print(f"  STATUS {response.status_code}\n  {response.text}")
+            continue
+
+        # Success
+        print("Got access token!")
+        access_token_json = response.json()
